ec371a2bf784d641a3e2c286b57849fb8043f3a3
Guilherme Leoi <geeleonidas@pm.me>
2025-08-18 17:15:07 -0300

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,106 @@
+# SPDX-FileCopyrightText: 2025 Guilherme Leoi <leoi.guilherme@aluno.ufabc.edu.br>
+#
+# SPDX-License-Identifier: 0BSD OR CC0-1.0
+
+{
+  description = "C/C++ Template";
+
+  inputs = {
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              # Markdown
+              lychee = {
+                enable = true;
+                settings.flags = "--exclude-path LICENSES/";
+              };
+              markdownlint.enable = true;
+
+              # C
+              clang-format = rec {
+                enable = true;
+                types_or = pkgs.lib.mkForce [
+                  "c"
+                  "c++"
+                ];
+                package = pkgs.clang-tools;
+                entry = "${package}/bin/clang-format -style=file -i";
+              };
+              clang-tidy.enable = true;
+
+              # Nix
+              nixfmt-rfc-style.enable = true;
+              flake-checker.enable = true;
+              statix.enable = true;
+
+              # Git
+              convco.enable = true;
+
+              # Licensing
+              reuse.enable = true;
+
+              # Security
+              ripsecrets.enable = true;
+
+              # EOFs and newlines
+              end-of-file-fixer.enable = true;
+              mixed-line-endings.enable = true;
+              trim-trailing-whitespace.enable = true;
+
+              # File system
+              check-symlinks.enable = true;
+              check-case-conflicts.enable = true;
+              check-added-large-files.enable = true;
+            };
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = nixpkgs.legacyPackages.${system}.mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+            buildInputs =
+              (with pkgs; [
+                clang
+
+                lldb
+                valgrind-light
+                linuxPackages_latest.perf
+              ])
+              ++ self.checks.${system}.pre-commit-check.enabledPackages;
+          };
+        }
+      );
+    };
+}
---
+++ src/main.c
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Guilherme Leoi <leoi.guilherme@aluno.ufabc.edu.br>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
+int main(int argc, char **argv) {
+  // TODO
+  return 0;
+}
