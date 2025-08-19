45d6c5b24d74215ea93514e41219f6877dc9ca2b
Guilherme Leoi <geeleonidas@pm.me>
2025-08-18 23:31:37 -0300

    feat(sequential): add blur filter

--- src/sequential.c
+++ src/sequential.c
@@ -38,11 +38,42 @@ size_t r_pixel(PpmImage *image, size_t m, size_t x, size_t y) {
   return (((size_t)(sum * 255)) % m) + 1;
 }
 
+int blur_at(PpmImage *image, size_t m, size_t x, size_t y) {
+  if (image == NULL || image->color_values == NULL)
+    return 0;
+  if (x > image->width || y > image->height)
+    return 0;
+  size_t idx = x + y * image->width;
+  size_t radius = r_pixel(image, m, x, y);
+  float sum_r = 0.0, sum_g = 0.0, sum_b = 0.0;
+  for (long i = -radius; i <= radius; i++) {
+    for (long j = -radius; j <= radius; j++) {
+      if (-i > x || -j > y)
+        continue;
+      if ((x + i) > image->width || (y + j) > image->height)
+        continue;
+      size_t neighbour_idx = (x + i) + (y + j) * image->width;
+      sum_r += image->color_values[neighbour_idx].r;
+      sum_g += image->color_values[neighbour_idx].g;
+      sum_b += image->color_values[neighbour_idx].b;
+    }
+  }
+  float n = (float)(1 + radius * 2);
+  image->color_values[idx].r = sum_r / n;
+  image->color_values[idx].g = sum_g / n;
+  image->color_values[idx].b = sum_b / n;
+  return 1;
+}
+
 int filter_ppm_image(PpmImage *image) {
   if (image == NULL || image->color_values == NULL)
     return 0;
   if (!grayscale(image))
     return 0;
+  for (size_t x = 0; x < image->width; x++)
+    for (size_t y = 0; y < image->height; y++)
+      if (!blur_at(image, 5, x, y))
+        return 0;
   // TODO
   return 1;
 }
