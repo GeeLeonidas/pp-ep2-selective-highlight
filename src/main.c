f17e6e8a35825e94a62ec0262e0b4750a7b46a8e
Guilherme Leoi <geeleonidas@pm.me>
2025-08-20 17:13:40 -0300

    feat: print error messages inside main and ppm parser code

--- src/main.c
+++ src/main.c
@@ -4,15 +4,41 @@
 
 #include "filter.h"
 #include "ppm.h"
+#include <stdio.h>
 #include <stdlib.h>
 
+#define ASSERT(expr, msg, exit_label)                                          \
+  if (!(expr)) {                                                               \
+    puts(msg);                                                                 \
+    goto exit_label;                                                           \
+  }
+
 int main(int argc, char **argv) {
+  ASSERT(argc >= 6, "Missing arguments (min.: 5)", exit);
   int exit_code = EXIT_FAILURE;
-  PpmImage *image = read_ppm_image(stdin);
-  if (!filter_ppm_image(image))
-    goto exit;
-  if (!save_ppm_image(image, stdout))
-    goto exit;
+  FILE *source_file = fopen(argv[1], "r");
+  ASSERT(source_file != NULL, "Error opening the source file", exit);
+  PpmImage *image = read_ppm_image(source_file);
+  ASSERT(fclose(source_file) == 0, "Error closing the source file", exit);
+  source_file = NULL;
+  ASSERT(image != NULL, "Error reading the PPM image", exit);
+  size_t m, raw_threshold;
+  float sharpen_factor, threshold;
+  ASSERT(sscanf(argv[3], "%lu", &m),
+         "Error reading variable radius' `m` integer", exit);
+  ASSERT(sscanf(argv[4], "%lu", &raw_threshold),
+         "Error reading sharpen's `threshold` integer", exit);
+  ASSERT(sscanf(argv[5], "%f", &sharpen_factor),
+         "Error reading sharpen's `sharpen_factor` float", exit);
+  threshold = ((float)raw_threshold) / ((float)image->max_value);
+  ASSERT(filter_ppm_image(image, threshold, sharpen_factor, m),
+         "Error applying the filter to the PPM image", exit);
+  FILE *output_file = fopen(argv[2], "w");
+  ASSERT(output_file != NULL, "Error opening the output file", exit);
+  ASSERT(save_ppm_image(image, output_file), "Error saving the PPM image",
+         exit);
+  ASSERT(fclose(output_file) == 0, "Error closing the output file", exit);
+  output_file = NULL;
   exit_code = EXIT_SUCCESS;
 exit:
   free_ppm_image(&image);
--- src/ppm.c
+++ src/ppm.c
@@ -6,33 +6,44 @@
 #include <math.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
+#define ASSERT(expr, msg, exit_label)                                          \
+  if (!(expr)) {                                                               \
+    puts(msg);                                                                 \
+    goto exit_label;                                                           \
+  }
+
 PpmImage *read_ppm_image(FILE *source_file) {
-  if (source_file == NULL)
-    return NULL;
+  ASSERT(source_file != NULL, "Source file is NULL", read_ppm_image_error);
   PpmImage *image = malloc(sizeof(PpmImage));
-  if (!fscanf(source_file, "P3"))
-    goto read_ppm_image_error;
-  if (!fscanf(source_file, "%lu %lu", &image->width, &image->height))
-    goto read_ppm_image_error;
-  if (!fscanf(source_file, "%hu", &image->max_value))
-    goto read_ppm_image_error;
+  char header[2];
+  ASSERT(fscanf(source_file, "%c%c", &header[0], &header[1]),
+         "Error reading the file header", read_ppm_image_error);
+  ASSERT(header[0] == 'P' && header[1] == '3',
+         "Unsupported format (expected `P3`)", read_ppm_image_error)
+  ASSERT(fscanf(source_file, "%lu %lu", &image->width, &image->height),
+         "Error reading `width` and `height` integers", read_ppm_image_error);
+  ASSERT(fscanf(source_file, "%hu", &image->max_value),
+         "Error reading `max_value` integer", read_ppm_image_error);
   size_t image_size = image->width * image->height;
   image->color_values_write = malloc(image_size * sizeof(RgbTriplet));
   image->color_values_read = malloc(image_size * sizeof(RgbTriplet));
   image->needs_flushing = 0;
   for (size_t idx = 0; idx < image_size; idx++) {
     uint16_t red, green, blue;
-    if (!fscanf(source_file, "%hu %hu %hu", &red, &green, &blue))
-      goto read_ppm_image_error;
+    ASSERT(fscanf(source_file, "%hu %hu %hu", &red, &green, &blue),
+           "Error reading `red`, `blue` and `green` integers",
+           read_ppm_image_error);
     RgbTriplet rgb =
         (RgbTriplet){.r = ((float)red) / ((float)image->max_value),
                      .g = ((float)green) / ((float)image->max_value),
                      .b = ((float)blue) / ((float)image->max_value)};
     write_at_idx_ppm_image(image, idx, rgb);
   }
-  flush_ppm_image(image);
+  ASSERT(flush_ppm_image(image), "Error flushing the image write buffer",
+         read_ppm_image_error);
   return image;
 read_ppm_image_error:
   free_ppm_image(&image);
@@ -81,24 +92,30 @@ int read_at_xy_ppm_image(PpmImage *image, size_t x, size_t y, RgbTriplet *rgb) {
 }
 
 int save_ppm_image(PpmImage *image, FILE *output_file) {
-  if (!fprintf(output_file, "P3\n"))
-    return 0;
-  if (!fprintf(output_file, "%lu %lu\n", image->width, image->height))
-    return 0;
-  if (!fprintf(output_file, "%hu\n", image->max_value))
-    return 0;
+  ASSERT(image != NULL, "PPM image is NULL", save_ppm_image_error);
+  ASSERT(output_file != NULL, "Output file is NULL", save_ppm_image_error);
+  ASSERT(fprintf(output_file, "P3\n"), "Error writing PPM image header",
+         save_ppm_image_error);
+  ASSERT(fprintf(output_file, "%lu %lu\n", image->width, image->height),
+         "Error writing `width` and `height` integers", save_ppm_image_error);
+  ASSERT(fprintf(output_file, "%hu\n", image->max_value),
+         "Error writing `max_value` integer", save_ppm_image_error);
   size_t image_size = image->width * image->height;
   for (int idx = 0; idx < image_size; idx++) {
     RgbTriplet rgb;
-    read_at_idx_ppm_image(image, idx, &rgb);
+    ASSERT(read_at_idx_ppm_image(image, idx, &rgb),
+           "Error reading at index from PPM image", save_ppm_image_error);
     uint16_t red, green, blue;
     red = (uint16_t)roundf(rgb.r * ((float)image->max_value));
     green = (uint16_t)roundf(rgb.g * ((float)image->max_value));
     blue = (uint16_t)roundf(rgb.b * ((float)image->max_value));
-    if (!fprintf(output_file, "%hu %hu %hu\n", red, green, blue))
-      return 0;
+    ASSERT(fprintf(output_file, "%hu %hu %hu\n", red, green, blue),
+           "Error writing `red`, `green` and `blue` integers",
+           save_ppm_image_error);
   }
   return 1;
+save_ppm_image_error:
+  return 0;
 }
 
 void free_ppm_image(PpmImage **image) {
