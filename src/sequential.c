f4c28d7db1994cb283ac022bb4dadd6f070a4007
Guilherme Leoi <geeleonidas@pm.me>
2025-08-19 22:13:14 -0300

    feat(sequential): add sharpen filter

--- src/sequential.c
+++ src/sequential.c
@@ -20,6 +20,7 @@ int grayscale(PpmImage *image) {
     if (!write_at_idx_ppm_image(image, idx, grayscale_rgb))
       return 0;
   }
+  flush_ppm_image(image);
   return 1;
 }
 
@@ -36,7 +37,7 @@ size_t r_pixel(PpmImage *image, size_t m, size_t x, size_t y) {
   return (((size_t)(sum * 255)) % m) + 1;
 }
 
-int blur_at(PpmImage *image, size_t m, size_t x, size_t y) {
+int blur_at(PpmImage *image, size_t m, size_t x, size_t y, RgbTriplet *rgb) {
   if (image == NULL)
     return 0;
   if (x > image->width || y > image->height)
@@ -60,8 +61,38 @@ int blur_at(PpmImage *image, size_t m, size_t x, size_t y) {
     }
   }
   float n = (float)(1 + radius * 2);
-  RgbTriplet rgb = (RgbTriplet){.r = sum_r / n, .g = sum_g / n, .b = sum_b / n};
-  if (!write_at_idx_ppm_image(image, idx, rgb))
+  *rgb = (RgbTriplet){.r = sum_r / n, .g = sum_g / n, .b = sum_b / n};
+  return 1;
+}
+
+float clamp_zero_one(float input) {
+  return (input >= 1.0) ? 1.0 : ((input <= 0.0) ? 0.0 : input);
+}
+
+int sharpen(PpmImage *image, float threshold, float sharpen_factor, size_t m) {
+  if (image == NULL)
+    return 0;
+  size_t image_size = image->width * image->height;
+  for (size_t x = 0; x < image->width; x++) {
+    for (size_t y = 0; y < image->height; y++) {
+      RgbTriplet rgb, blur, new_rgb;
+      if (!read_at_xy_ppm_image(image, x, y, &rgb))
+        return 0;
+      if (!blur_at(image, m, x, y, &blur))
+        return 0;
+      if (rgb.r <= threshold)
+        new_rgb = (RgbTriplet){
+            .r = roundf(blur.r), .g = roundf(blur.g), .b = roundf(blur.b)};
+      else
+        new_rgb = (RgbTriplet){
+            .r = clamp_zero_one(rgb.r + sharpen_factor * (rgb.r - blur.r)),
+            .g = clamp_zero_one(rgb.g + sharpen_factor * (rgb.g - blur.g)),
+            .b = clamp_zero_one(rgb.b + sharpen_factor * (rgb.b - blur.b))};
+      if (!write_at_xy_ppm_image(image, x, y, new_rgb))
+        return 0;
+    }
+  }
+  if (!flush_ppm_image(image))
     return 0;
   return 1;
 }
@@ -71,10 +102,7 @@ int filter_ppm_image(PpmImage *image) {
     return 0;
   if (!grayscale(image))
     return 0;
-  for (size_t x = 0; x < image->width; x++)
-    for (size_t y = 0; y < image->height; y++)
-      if (!blur_at(image, 5, x, y))
-        return 0;
-  // TODO
+  if (!sharpen(image, 0.5, 1.0, 5))
+    return 0;
   return 1;
 }
