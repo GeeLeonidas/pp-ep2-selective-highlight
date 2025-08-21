5bb6acd141066201132eb1cee47e50394574a661
Guilherme Leoi <geeleonidas@pm.me>
2025-08-21 11:36:20 -0300

    docs: add comments to main

--- src/main.c
+++ src/main.c
@@ -21,12 +21,14 @@ int main(int argc, char **argv) {
   ASSERT(output_file != NULL, "Error opening the output file", exit);
   ASSERT(fclose(output_file) == 0, "Error closing the output file", exit);
   output_file = NULL;
+  // Opens the source file and reads the PPM image
   FILE *source_file = fopen(argv[1], "r");
   ASSERT(source_file != NULL, "Error opening the source file", exit);
   PpmImage *image = read_ppm_image(source_file);
   ASSERT(fclose(source_file) == 0, "Error closing the source file", exit);
   source_file = NULL;
   ASSERT(image != NULL, "Error reading the PPM image", exit);
+  // Reads the runtime parameters
   size_t m, raw_threshold;
   float sharpen_factor, threshold;
   ASSERT(sscanf(argv[3], "%lu", &m),
@@ -36,8 +38,10 @@ int main(int argc, char **argv) {
   ASSERT(sscanf(argv[5], "%f", &sharpen_factor),
          "Error reading sharpen's `sharpen_factor` float", exit);
   threshold = ((float)raw_threshold) / ((float)image->max_value);
+  // Apply the PPM image filter
   ASSERT(filter_ppm_image(image, threshold, sharpen_factor, m),
          "Error applying the filter to the PPM image", exit);
+  // Saves the PPM image to the output file
   output_file = fopen(argv[2], "w");
   ASSERT(output_file != NULL, "Error opening the output file", exit);
   ASSERT(save_ppm_image(image, output_file), "Error saving the PPM image",
