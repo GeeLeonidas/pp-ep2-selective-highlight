a2d6924927b97c3556263c8545db9c7da76536ae
Guilherme Leoi <geeleonidas@pm.me>
2025-08-28 18:07:21 -0300

    feat(tooling): add runner and benchmark scripts

---
+++ tools/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+# SPDX-FileCopyrightText: 2025 Guilherme Leoi <leoi.guilherme@aluno.ufabc.edu.br>
+#
+# SPDX-License-Identifier: 0BSD OR CC0-1.0
+
+./target/release/$1 "$2" /dev/null $3 $4 $5 $6
