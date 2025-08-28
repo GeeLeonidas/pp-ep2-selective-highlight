0f40a168ced37c49ffa5971771f39101e24deba5
Guilherme Leoi <geeleonidas@pm.me>
2025-08-28 09:51:41 -0300

    fix(tooling): use correct directories

--- tools/build_cuda.sh
+++ tools/build_cuda.sh
@@ -3,13 +3,13 @@
 #
 # SPDX-License-Identifier: 0BSD OR CC0-1.0
 
-rm -f target/debug/cuda
-mkdir -p target/debug/
+rm -f target/release/cuda
+mkdir -p target/release/
 
 CUDA_PATH="$(find /usr/local -regex '.+/cuda-[0-9]+\.[0-9]+$')"
 PATH="$CUDA_PATH/bin:$PATH"
 LD_LIBRARY_PATH="$CUDA_PATH/lib64:$LD_LIBRARY_PATH"
 
 echo "Compiling cuda variant with nvcc..."
-nvcc src/cuda.cu -O3 -o target/build/cuda
+nvcc src/cuda.cu -O3 -o target/release/cuda
 echo "Variant was compiled!"
