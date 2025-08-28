fcafd5c86e9f5695e1c15f26721b9df4ee9a9ebe
Guilherme Leoi <geeleonidas@pm.me>
2025-08-27 23:17:02 -0300

    feat(tooling): add cuda-specific build scripts

---
+++ tools/build_debug_cuda.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# SPDX-FileCopyrightText: 2025 Guilherme Leoi <leoi.guilherme@aluno.ufabc.edu.br>
+#
+# SPDX-License-Identifier: 0BSD OR CC0-1.0
+
+rm -f target/debug/cuda
+mkdir -p target/debug/
+
+echo "Compiling cuda variant with nvcc..."
+nvcc src/main.c src/ppm.c src/cuda.cu -lm -g \
+    -o target/debug/cuda
+echo "Variant was compiled!"
